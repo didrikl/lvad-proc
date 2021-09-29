7a25062a83eb69c3fe7f07e65bda396b454d62de
Didrik Lilja <didrik.lilja@hotmail.com>
2021-09-29 20:49:42 +0200

    test
    test

--- Untitled.m
+++
@@ -1,9 +0,0 @@
-t = 0:0.001:2;
-x = chirp(t,100,1,200,'quadratic');
-[s,f,t,p] = spectrogram(x,128,120,128,1e3);
-figure(1)
-sh = surf(p);
-view(0, 90)
-axis tight
-yt = get(gca, 'YTick')
-set(gca, 'YTick',yt, 'YTickLabel',yt*1E+4)
--- Untitled8.m
+++
@@ -1,6 +0,0 @@
-test = ["as", "b";]
-for s=test
-    disp(s)
-    disp hei
-end
-
